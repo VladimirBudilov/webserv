04b2f9a6a8a1a0b19bd4935d78a5860b2893cf6d
Dmitrii Chapaev <chapaev.dima@mail.ru>
2023-09-25 15:56:19 +0400

    http request v0.11

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -6,6 +6,25 @@
 class Request {
 public:
     std::string RequestData;
+    bool hasCGI();
+    void parse_request(const std::string &request);
+
+    const std::string &getMethod() const;
+
+    const std::string &getPath() const;
+
+    bool isVersion() const;
+
+    const std::map<std::string, std::string> &getHeaders() const;
+
+    const std::string &getBody() const;
+
+private:
+    std::string method;
+    std::string path;
+    bool version;
+    std::map<std::string, std::string> headers;
+    std::string body;
 };
 
 
--- sources/HTTP/Request.cpp
+++ sources/HTTP/Request.cpp
@@ -1,1 +1,49 @@
 #include "Request.hpp"
+
+bool Request::hasCGI() {
+    //TODO add more validation
+    if(RequestData.find(".py") != std::string::npos)
+        return true;
+    return false;
+}
+
+void Request::parse_request(const std::string &request) {
+    std::string line = request.substr(0, request.find("\r\n"));
+    std::stringstream ss(line);
+    std::string ver;
+    ss >> this->method >> this->path >> ver;
+    if (ver == "HTTP/1.1")
+        this->version = true;
+    else
+        this->version = false;
+    size_t pos = request.find("\r\n\r\n");
+    size_t i = 0;
+    while (i < pos) {
+        line = request.substr(i, request.find("\r\n", i) - i);
+        i += line.size() + 2;
+        size_t p = line.find(": ");
+        if (p != std::string::npos)
+            this->headers.insert(std::make_pair(line.substr(0, p), line.substr(p + 2)));
+    }
+    this->body = request.substr(pos + 4);
+}
+
+const std::string &Request::getMethod() const {
+    return method;
+}
+
+const std::string &Request::getPath() const {
+    return path;
+}
+
+bool Request::isVersion() const {
+    return version;
+}
+
+const std::map<std::string, std::string> &Request::getHeaders() const {
+    return headers;
+}
+
+const std::string &Request::getBody() const {
+    return body;
+}
